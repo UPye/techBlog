a0fc1eebe106f2b719798496aca830180db26e41
UPye <Urika.Pye@gmail.com>
2021-06-16 06:58:11 -0400

    FINALLY got homepage to render. No seeds as of yet to test routes, but terminal is not showing any errors. Will test routes in Insomnia next.

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
-// import express sessions
+// Import handlebars
+
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
+
+// Import sessions
+
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
@@ -11,36 +18,24 @@ const sess = {
     cookie: {},
     resave: false,
     saveUninitialized: true,
-    store: new SequelizeStore ({
+    store: new SequelizeStore({
         db: sequelize
     })
 };
 
 const app = express();
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 // turn routes on
 app.use(routes);
 
-// turn connection to db and server on
+// turn db and server connection on
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
-
-/*
-//IMPORT HANDLEBARS
-const exphbs = require('express-handlebars');
-
-
-//IMPORT HANDLEBARS HELPERS
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
-
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-*/
